df68dd3484f0ffbecc7125578cb1c2d6fcb3cf7f
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-02-04 12:04:37 -0300

    LocalStorage

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
@@ -32,7 +32,9 @@ function getSkuFromProductItem(item) {
 function cartItemClickListener(event) {
   const targ = event.target;
   cart.removeChild(targ);
+  localStorage.setItem('cartItems', cart.innerHTML);
 }
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -40,6 +42,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
 const buttonAddToShip = async (id) => {
   const items = await fetchItem(id);
   const data = {
@@ -47,12 +50,12 @@ const buttonAddToShip = async (id) => {
     name: items.title,
     salePrice: items.price,
   };
-  cart.appendChild(createCartItemElement(data)); 
+  cart.appendChild(createCartItemElement(data));
+  localStorage.setItem('cartItems', cart.innerHTML);
 };
 const shipCart = async (e) => {
   const param = await getSkuFromProductItem(e.target.parentNode);
   buttonAddToShip(param);
-  localStorage.setItem('cartItems', cart.innerHTML);
 };
 
 const createItems = async (item) => {
@@ -74,13 +77,24 @@ const createItems = async (item) => {
 function empCart() {
   const carrinho = document.querySelectorAll('.cart__items');
   carrinho[0].innerHTML = '';
+  localStorage.setItem('cartItems', cart.innerHTML);
+}
+async function totalPrice() {
+  await fetchItem();
+  const price = cart.innerHTML.split('$');
+  console.log(price);
 }
 
 window.onload = async () => {
-  await createItems('Fusca');
+  await createItems('computador');
   const loading = document.querySelector('.loading');
   loading.remove();
   cart.innerHTML = localStorage.getItem('cartItems');
+  const cartList = document.querySelectorAll('.cart__item');
+  cartList.forEach((list) => {
+    list.addEventListener('click', cartItemClickListener);
+  });
   const btn = document.querySelector('.empty-cart');
   btn.addEventListener('click', empCart);
+  totalPrice();
 };
